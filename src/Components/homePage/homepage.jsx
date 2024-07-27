b92c87ac848917367de580c48cc65751d3797bf1
visionmax01 <121842628+visionmax01@users.noreply.github.com>
2024-07-27 15:44:34 +0530

    gdf
    fgdg

--- src/Components/homePage/homepage.jsx
+++ src/Components/homePage/homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
             <NavBar/>
 
               <div className='bg-white w-1/2 h-96 mx-auto'>
-                   jkrhdsgklh dgb,jfdhgjkd
+                   jkrhdsgklh dgb,jfdhgjkd fgfgh
               </div>
         </div>
     );
