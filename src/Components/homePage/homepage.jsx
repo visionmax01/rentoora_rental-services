697c0589a8763818fcf16f3027d8cb646cb58ed7
visionmax01 <121842628+visionmax01@users.noreply.github.com>
2024-07-28 20:38:34 +0530

    Update homepage.jsx

--- src/Components/homePage/homepage.jsx
+++ src/Components/homePage/homepage.jsx
@@ -308,7 +308,7 @@ const Homepage = () => {
       {/* section Two  Started From  here  */}
 
       <div className="relative   text-white h-96 ">written  by shav raj</div>
-      <div className="  text-black h-96 ">dfhhgjfdh</div>
+      <div className="relative   text-white h-96 ">written  by bhishan sah</div>
 
       {/* footer section  */}
       <Footer />
