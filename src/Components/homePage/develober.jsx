302abc087cfe0cb264188eb0c261c71ef5bc369d
Bhishan Sah <121842628+visionmax01@users.noreply.github.com>
2024-09-25 17:52:48 +0530

    servies developer and about page added

--- src/Components/homePage/develober.jsx
+++ src/Components/homePage/develober.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import BhishanImage from "../../assets/img/my_profile_pic.jpg"; 
-import SecondDevImage from "../../assets/img/shiv.png"; 
+import BhishanImage from "../../assets/img/my_profile_pic.jpg";
+import SecondDevImage from "../../assets/img/shiv.jpg";
 import NavBar from "../NavBar";
 
 const Developer = () => {
@@ -12,7 +12,13 @@ const Developer = () => {
       mobile: "+91 9728161275",
       nepalino: "+977 9819931223",
       email: "bhishansah@gmail.com",
+      website: "https://www.bhishansah.com.np",
       image: BhishanImage,
+      socialLinks: {
+        facebook: "https://www.facebook.com/share/qJntLSmzjo3uB8HV/?mibextid=qi2Omg",
+        whatsapp: "https://wa.me/919728161275?text=Hello!%20How%20are%20you.",
+        linkedin: "https://www.linkedin.com/in/bhishansah/",
+      },
     },
     {
       name: "SHIV RAJ RAUT",
@@ -21,52 +27,97 @@ const Developer = () => {
       mobile: "+91- not available",
       nepalino: "+977 9819931223",
       email: "sraut683@rku.ac.in",
+      website: "https://www.shivrajraut.com.np",
       image: SecondDevImage,
+      socialLinks: {
+        facebook: "https://www.facebook.com/shiv.raja.9404",
+        whatsapp: "https://wa.me/9779825823297?text=Welcome%20to%20Shiv%20Raj%20account",
+        linkedin: "https://www.linkedin.com/in/shiv-raut-73aa11254/",
+      },
     },
   ];
 
   return (
-   <>
-   <NavBar/>
-   <div className="flex flex-col items-center  p-8">
-      <h1 className="md:text-4xl text-2xl text-white font-bold mb-8">
-        Meet Our Developers
-      </h1>
-      <div className="w-full  flex justify-center flex-wrap gap-14 ">
-        {developers.map((dev, index) => (
-          <div
-            key={index}
-            className="w-[95%] md:w-72   bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
-          >
-            <img
-              src={dev.image}
-              alt={`${dev.name}`}
-              className="w-full h-60 md:object-cover md:object-top  "
-            />
-            <div className="p-6">
-              <h2 className="text-xl font-bold text-gray-800 mb-2">
-                {dev.name}
-              </h2>
-              <p className="text-sm text-gray-600">{dev.title}</p>
-              <p className="text-sm text-gray-600 mb-4">{dev.role}</p>
-              <p className="text-sm text-gray-800">
-                <strong>Indian No:</strong> {dev.mobile}
-              </p>
-              <p className="text-sm text-gray-800">
-                <strong>Nepali No:</strong> {dev.nepalino}
-              </p>
-              <p className="text-sm text-gray-800">
-                <strong>Email:</strong> {dev.email}
-              </p>
+    <div className="md:h-screen bg-gray-50">
+      <NavBar />
+      <div className="flex flex-col items-center p-8">
+        <h1 className="md:text-4xl text-2xl text-black font-bold mb-8">
+          Meet Our Developers
+          <p className="md:w-48 w-32 h-1 mt-1 rounded-full bg-red-600 mx-auto"></p>
+        </h1>
+        <div className="w-full relative flex justify-center flex-wrap gap-14">
+          {developers.map((dev, index) => (
+            <div
+              key={index}
+              className="w-[95%] md:w-72 bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
+            >
+              <i className="fa-sharp-duotone fa-solid fa-bookmark absolute right-4 -top-1.5 text-4xl text-red-600"></i>
+              <img
+                src={dev.image}
+                alt={`${dev.name}`}
+                className="w-full h-60 md:object-fit md:object-top object-fill"
+              />
+              <div className=" w-full flex justify-between ">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold text-gray-800">
+                    {dev.name}
+                  </h2>
+                  <p className="text-sm text-gray-600">{dev.title}</p>
+                  <p className="text-sm text-gray-600 mb-4">{dev.role}</p>
+                  <p className="text-sm text-gray-800 mb-2">
+                    <strong>Indian No:</strong> {dev.mobile}
+                  </p>
+                  <p className="text-sm text-gray-800 mb-2">
+                    <strong>Nepali No:</strong> {dev.nepalino}
+                  </p>
+                  <p className="text-sm text-gray-800">
+                    <strong>Email:</strong> {dev.email}
+                  </p>
+                  <div className="mt-2">
+                    <strong>Website : </strong>
+                  <a
+                    href={dev.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=" text-red-700 hover:text-brand-dark rounded"
+                  >
+                   <i class="fa-solid fa-globe"></i>&nbsp;Portfolio website
+                  </a>
+                  </div>
+                </div>
+                <div className="p-2 bg-gray-200 py-6 flex flex-col space-y-3">
+                  <a
+                    href={dev.socialLinks.facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-blue-700"
+                  >
+                    <i className="fa-brands fa-square-facebook text-3xl hover:text-blue-700"></i>
+                  </a>
+                  <a
+                    href={dev.socialLinks.whatsapp}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-green-500"
+                  >
+                    <i className="fa-brands fa-square-whatsapp text-3xl hover:text-green-700"></i>
+                  </a>
+                  <a
+                    href={dev.socialLinks.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-blue-500"
+                  >
+                    <i className="fa-brands fa-linkedin text-3xl hover:text-blue-700"></i>
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-   </>
   );
 };
 
 export default Developer;
-
-
--- src/Components/homePage/services.jsx
+++ src/Components/homePage/services.jsx
@@ -13,105 +13,107 @@ const Services = () => {
 
 
     return (
-        <div className="text-black   uppercase rounded-t-3xl md:rounded-none  md:text-lg font-bold  text-[12px] absolute z-10 p-4 top-28 h-auto md:min-h-80 min-h-96  w-full   flex gap-2 justify-around   items-center flex-wrap md:justify-center md:gap-16 bg-gradient-to-r from-gray-700 to-blue-950 bluer-500 ">
-        <h1 className="text-center absolute top-2  md:top-4  text-white text-xl md:text-4xl opacity-70">
-          Services We Provide   
-        </h1>
-
-        <div
-          onClick={HandleRoomRent}
-          className="relative  hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0  bg-blue-300   rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={RoomIcon}
-            className="w-12 h-12 md:w-16 md:h-16 z-10 relative "
-            alt=""
-            srcset=""
-          />
-          <span className="relative text-[14px] mt-2 z-10 break-words">
-            Rent Room
-          </span>
+      <div className="text-black flex flex-col items-center  uppercase rounded-t-3xl md:rounded-none  md:text-lg font-bold  text-[12px] absolute z-10 p-4 top-28 h-auto md:min-h-80 min-h-96  w-full      bg-gradient-to-r from-gray-700 to-blue-950 bluer-500">
+        <div>
+          <h1 className='md:text-3xl text-xl  text-white opacity-75 mb-4 uppercase font-extrabold'>Services We Provide</h1>
         </div>
+       <div className="flex gap-4 justify-around items-center flex-wrap md:justify-center md:gap-16">
+       <div
+         onClick={HandleRoomRent}
+         className="relative  hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0  bg-blue-300   rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={RoomIcon}
+           className="w-12 h-12 md:w-16 md:h-16 z-10 relative "
+           alt=""
+           srcset=""
+         />
+         <span className="relative text-[14px] mt-2 z-10 break-words">
+           Rent Room
+         </span>
+       </div>
 
-        <div
-          onClick={HandleRoomRent}
-          className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0  bg-blue-300    rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={Apartment}
-            className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
-            alt=""
-            srcset=""
-          />
-          <span className="relative z-10 text-[14px] mt-2 ">
-            Apartment
-          </span>
-        </div>
+       <div
+         onClick={HandleRoomRent}
+         className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0  bg-blue-300    rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={Apartment}
+           className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
+           alt=""
+           srcset=""
+         />
+         <span className="relative z-10 text-[14px] mt-2 ">
+           Apartment
+         </span>
+       </div>
 
-        <div
-          onClick={HandleRoomRent}
-          className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={HomeIcon}
-            className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
-            alt=""
-            srcset=""
-          />
-          <span className="relative z-10 text-[14px] mt-2">
-            Rent Home
-          </span>
-        </div>
+       <div
+         onClick={HandleRoomRent}
+         className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={HomeIcon}
+           className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
+           alt=""
+           srcset=""
+         />
+         <span className="relative z-10 text-[14px] mt-2">
+           Rent Home
+         </span>
+       </div>
 
-        <div
-          onClick={HandleRoomRent}
-          className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={PlumberIcon}
-            className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
-            alt=""
-            srcset=""
-          />
-          <span className="relative z-10 text-[14px] mt-2">Plumber</span>
-        </div>
+       <div
+         onClick={HandleRoomRent}
+         className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={PlumberIcon}
+           className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
+           alt=""
+           srcset=""
+         />
+         <span className="relative z-10 text-[14px] mt-2">Plumber</span>
+       </div>
 
-        <div
-          onClick={HandleRoomRent}
-          className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={Electrician}
-            className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
-            alt=""
-            srcset=""
-          />
-          <span className="relative z-10 text-[14px] mt-2">
-            Electrician
-          </span>
-        </div>
+       <div
+         onClick={HandleRoomRent}
+         className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={Electrician}
+           className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
+           alt=""
+           srcset=""
+         />
+         <span className="relative z-10 text-[14px] mt-2">
+           Electrician
+         </span>
+       </div>
 
-        <div
-          onClick={HandleRoomRent}
-          className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
-        >
-          <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
-          <img
-            src={Socialphone}
-            className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
-            alt=""
-            srcset=""
-          />
-          <span className="relative z-10 text-[12px] mt-2">
-            Sell your Phone
-          </span>
-        </div>
+       <div
+         onClick={HandleRoomRent}
+         className="relative hover:-translate-y-2 duration-300 ease-in-out  flex flex-col justify-center items-center w-28 h-28 md:w-32 md:h-32 bg-gray-100 bg-opacity-75 cursor-pointer rounded-lg shadow-gray-300 shadow-md overflow-hidden group"
+       >
+         <div className="absolute inset-0 bg-blue-300     rounded-tl-full transform scale-0 group-hover:animate-fill-bottom-right hover:rounded-tl-fully"></div>
+         <img
+           src={Socialphone}
+           className="w-12 h-12 md:w-16 md:h-16  z-10 relative"
+           alt=""
+           srcset=""
+         />
+         <span className="relative z-10 text-[12px] mt-2">
+           Sell your Phone
+         </span>
+       </div>
+     </div>
       </div>
+        
     );
 };
 
